1df76726eda4f800ee188f0f5c54cdcd8fde865f
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-04-24 13:47:38 +0000

    Recup serveur

--- mini-projet/public/create.php
+++ mini-projet/public/create.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../src/functions.php';
 // Définition des valeurs par défaut de l'animal de compagnie
 $name = $_POST["name"] ?? null;
 $species = $_POST["species"] ?? null;
+$nickname = $_POST["nickname"] ?? null;
 
 // Gestion de la soumission du formulaire
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
